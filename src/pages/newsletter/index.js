e5bb72a70485ff1d3feab932cde677e1ef552103
Kyle <kylekaplan50@gmail.com>
2021-02-01 22:53:31 -0500

    updated convertKitForm import

--- src/pages/newsletter/index.js
+++ src/pages/newsletter/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Layout from '@lekoarts/gatsby-theme-minimal-blog/src/components/layout'
 import SEO from '@lekoarts/gatsby-theme-minimal-blog/src/components/seo'
-import ConvertKitForm from './convertKitForm'
+import ConvertKitForm from './convertKitForm.js'
 
 const Newsletter = () => {
   return (
